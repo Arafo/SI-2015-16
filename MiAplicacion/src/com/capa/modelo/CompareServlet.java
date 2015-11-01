2c0765dd6892bd95635c2b8073af39cccf40215a
Arafo <rafa_marcen@hotmail.com>
2015-11-01 13:20:51 +0100

    Añadido jsp y servlet de la función de comparación (no muy funcional)

--- MiAplicacion/src/com/capa/modelo/CompareServlet.java
+++ MiAplicacion/src/com/capa/modelo/CompareServlet.java
@@ -1,15 +1,12 @@
 package com.capa.modelo;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.capa.persistencia.OracleConnector;
-import com.google.gson.Gson;
 
 public class CompareServlet extends HttpServlet {
 
@@ -19,21 +16,22 @@ public class CompareServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		OracleConnector f = new OracleConnector();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String obra1 = null;
+		String obra2 = null;
+
+		if (request.getParameter("obra1") != null)
+			obra1 = request.getParameter("obra1");
+		if (request.getParameter("obra2") != null)
+			obra2 = request.getParameter("obra2");
 		
-		String name = request.getParameter("term");
-		List<Obra> obras = f.getObras(name);
+		System.out.println(obra1);
+		System.out.println(obra2);
+		request.setAttribute("obra1", obra1);
+		request.setAttribute("obra2", obra2);
 
-		String json = new Gson().toJson(obras);
-		response.setContentType("application/json");
 
-		try {
-			response.getWriter().print(json);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println(json);
+		RequestDispatcher view = request.getRequestDispatcher("comparacion.jsp");
+		view.forward(request, response);
 	}
 }
